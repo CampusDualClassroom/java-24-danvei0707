c1716f4d9c71e124eca7e6eea44dc83d908592bb
Daniel <dani.v2509@outlook.com>
2024-10-21 16:35:48 +0200

    Solution

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -1,18 +1,40 @@
 package com.campusdual.classroom;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise24 {
+
+    protected static String[] data = {
+            "Smith",
+            "Montessori",
+            "Peralta",
+            "House"
+    };
+
     public static Queue<String> createQueue() {
-
+        return new LinkedList<String>(Arrays.asList(data));
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
+    public static void printAndEmptyQueue(Queue<String> q) {
+        System.out.println(q.peek() + "\n");
 
+        // Objetivo 3
+        System.out.println("Queue is empty? " + q.isEmpty());
+
+        while(!q.isEmpty()){
+            System.out.println("\tRemoved " + q.poll());
+        }
+        System.out.println("Queue is empty? " + q.isEmpty());
     }
 
     public static void main(String[] args) {
+        // Objetivo 1
+        Queue<String> q = createQueue();
 
+        // Objetivo 2
+        printAndEmptyQueue(q);
     }
+
 
 }
